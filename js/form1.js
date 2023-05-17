7486f98ec1cedd4775ecda823883d1ae7eeef465
bpcastro1 <63468443+bpcastro1@users.noreply.github.com>
2023-05-16 20:49:59 -0500

    commit

--- js/form1.js
+++ js/form1.js
@@ -1,45 +1,48 @@
-function Verificacion() {
+document.addEventListener('DOMContentLoaded', function() {
   var propietarioCheckbox = document.getElementById('propietario');
   var enviarButton = document.getElementById('enviar');
-  enviarButton.disabled = !propietarioCheckbox.checked;
-}
-window.addEventListener('DOMContentLoaded', function() {
-  var propietarioCheckbox = document.getElementById('propietario');
-  var enviarButton = document.getElementById('enviar');
-  enviarButton.disabled = !propietarioCheckbox.checked;
-  propietarioCheckbox.addEventListener('change', function() {
-    Verificacion();
-  });
-});
-function Mostrar() {
   var terceroCheckbox = document.getElementById('tercero');
   var confidentialInfoForm = document.getElementById('confidential-info-form');
-  if (terceroCheckbox.checked) {
-    confidentialInfoForm.style.display = 'block';
-  } else {
-    confidentialInfoForm.style.display = 'none';
+  var correoInput = document.getElementById('correo');
+  var telefonoInput = document.getElementById('telefono');
+
+  enviarButton.disabled = !propietarioCheckbox.checked;
+
+  propietarioCheckbox.addEventListener('change', function() {
+    enviarButton.disabled = !this.checked;
+  });
+
+  terceroCheckbox.addEventListener('change', function() {
+    confidentialInfoForm.style.display = this.checked ? 'block' : 'none';
+  });
+
+  function validar() {
+    var correo = correoInput.value;
+    var expresion = /^[a-z][\w.-]+@\w[\w.-]+\.[\w.-]*[a-z][a-z]$/i;
+
+    if (!expresion.test(correo)) {
+      alert("Ingrese un correo ");
+      return false;
+    }
+
+    var telefono = telefonoInput.value;
+    if (telefono.length !== 10 || isNaN(telefono)) {
+      alert("Ingrese un numero de telefono");
+      return false;
+    }
+
+    var boton = document.getElementById('enviar');
+    boton.disabled = true;
+    boton.value = 'Enviando datos';
+    this.form.submit();
+    return true;
   }
-}
-function validar() {
-  var correo = document.getElementById('correo').value;
-  var expresion = /^[a-z][\w.-]+@\w[\w.-]+\.[\w.-]*[a-z][a-z]$/i;
-  if (!expresion.test(correo)) {
-    alert("Ingrese un correo valido");
-    return false;
-  }
-  var telefono = document.getElementById('telefono').value;
-  if (telefono.length !== 10 || isNaN(telefono)) {
-    alert("Ingrese un telefono válido de 10 dígitos");
-    return false;
-  }
-  var boton = document.getElementById('enviar');
-  boton.disabled = true;
-  boton.value = 'Enviando datos........';
-  this.form.submit();
-  return true;
-}
-function borrarform() {
+
+  function borrarform() {
     document.getElementById("name").value = '';
     document.getElementById("descrip").value = '';
     document.getElementById("precio").value = '';
   }
+
+  enviarButton.addEventListener('click', validar);
+});
